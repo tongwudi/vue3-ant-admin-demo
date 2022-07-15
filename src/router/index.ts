66f800cdbf510499af0ec5ff89205106726d8bd3
tongwudi <litong@33.cn>
2022-07-15 09:50:30 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/login',
+    //   component: () => import(/* webpackChunkName: "about" */ '../layout/index.vue'),
+    //   children: [
+    //     {
+    //       path: 'dashboard',
+    //       name: 'Dashboard',
+    //       component: () => import('@/views/Dashboard/index.vue'),
+    //       meta: { title: '首页', icon: 'homepage' }
+    //     },
+    //   ]
+  },
+  {
+    path: '/login',
+    // name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
+  },
+]
+
+const router = createRouter({
+  // history: createWebHashHistory(),
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <a-button type="primary">Primary Button</a-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
